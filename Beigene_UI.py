39a8a6cb62fb39cbe6c1fedcef3c61c0297a71ea
Kartik <sunny.mehboobani@gmail.com>
2025-01-21 22:34:09 +0530

    Update Beigene_UI.py

--- Beigene_UI.py
+++ Beigene_UI.py
@@ -68,7 +68,7 @@ custom_css=f"""
 """
 
 st.markdown(custom_css,unsafe_allow_html=True)
-st.image('Header_logo.png')
+st.image('Header_logo.png', width = 150)
 
 main_cols = st.columns(8)
 with main_cols[7]:
